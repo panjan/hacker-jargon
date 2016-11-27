df4a53fd40ca5442f7f4dd0a354d128244840b75
Jan Zdráhal <hi@honza.me>
2016-11-27 13:25:11 +0100

    Create routes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// Top 10 most occurring words in the titles of the last 25 stories
+app.get('/story-titles', function (req, res) {
+  res.send('Hi!');
+});
+
+// Top 10 most occurring words in the titles of the post of exactly the last week
+app.get('/post-titles', function (req, res) {
+  res.send('Hi!');
+});
+
+// Top 10 most occurring words in titles of the last 600 stories of users with at least 10.000 karma
+app.get('/high-karma-stories', function (req, res) {
+  res.send('Hi!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000.');
+});
