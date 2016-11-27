386492f92f077f353f22eb98aa95665901bfd174
Jan Zdráhal <hi@honza.me>
2016-11-27 17:42:16 +0100

    Implement route /story-titles

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const app = express();
 
 // Top 10 most occurring words in the titles of the last 25 stories
 app.get('/story-titles', function (req, res) {
-  res.send({ words: topWords.inStoryTitles() });
+  topWords.inStoryTitles(res);
 });
 
 // Top 10 most occurring words in the titles of the post of exactly the last week
 app.get('/post-titles', function (req, res) {
-  res.send({ words: topWords.inPostTitles() });
+  topWords.inPostTitles(res);
 });
 
 // Top 10 most occurring words in titles of the last 600 stories of users with at least 10.000 karma
 app.get('/high-karma-stories', function (req, res) {
-  res.send({ words: topWords.inHighKarmaStoryTitles() });
+  topWords.inHighKarmaStoryTitles(res);
 });
 
 app.listen(3000, () => {
--- top-words.js
+++ top-words.js
@@ -1,11 +1,65 @@
-exports.inStoryTitles = () => {
-  return ['foo', 'bar'];
+const request = require('request');
+
+const baseAddress = 'https://hacker-news.firebaseio.com/v0';
+
+function getTitle(id) {
+  return new Promise((resolve, reject) => {
+    request.get(
+      `${baseAddress}/item/${id}.json`,
+      (error, response, body) => {
+        if (error) reject(error);
+        resolve(JSON.parse(body).title);
+      });
+  });
+}
+
+function getTitles(storyIds) {
+  const titlePromises = [];
+  for(let id of storyIds) {
+    titlePromises.push(getTitle(id));
+  }
+  return Promise.all(titlePromises);
+}
+
+function topWords(text) {
+  const unique = Array.from(new Set(text.split(' ')));
+  const frequencies = wordFrequency(text);
+  const topWords = Object.keys(frequencies)
+    .sort((a, b) => frequencies[b] - frequencies[a])
+    .slice(0, 10);
+  return { words: topWords };
+}
+
+// http://stackoverflow.com/a/30907349/2080939
+function wordFrequency(string) {
+  return string.replace(/[.]/g, '')
+    .split(/\s/)
+    .reduce((map, word) =>
+            Object.assign(map, {
+              [word]: (map[word])
+                ? map[word] + 1
+                : 1,
+            }),
+            {}
+           );
+}
+
+exports.inStoryTitles = (res) => {
+  request.get(
+    baseAddress + '/newstories.json',
+    (error, response, body) => {
+      if (error) res.send(error);
+      const ids = JSON.parse(body);
+      getTitles(ids.slice(0, 25))
+        .then((titles) => res.send(topWords(titles.join(' '))))
+        .catch((err) => res.send(err));
+    });
 };
 
-exports.inPostTitles = () => {
-  return ['foo', 'bar'];
+exports.inPostTitles = (res) => {
+  res.send({ words: ['foo', 'bar'] });
 };
 
-exports.inHighKarmaStoryTitles = () => {
-  return ['foo', 'bar'];
+exports.inHighKarmaStoryTitles = (res) => {
+  res.send({ words: ['foo', 'bar'] });
 };
